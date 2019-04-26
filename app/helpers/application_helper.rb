076c300465f0b1140c55366c599f9ffb119f5e61
aroma <aroma99959@gmail.com>
2019-04-26 10:58:15 +0000

    finish static page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    
+    #ページごとにタイトルを返す。
+    def full_title(page_title = "")
+        base_title = "勤怠管理システム"
+      if  base_title.empty?
+        base_title
+      else
+        page_title + " | " + base_title
+      end
+    end
 end
